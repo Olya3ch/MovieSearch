423449cfda3d2f3039f9805e85a8c4aa8f4ca8fe
Olya Tricolici <oleatrik@gmail.com>
2021-05-26 22:35:39 +0300

    shows posters links in html

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+let inputEl = document.querySelectorAll("input");
+inputEl.forEach((node) => (node.onchange = movieSearch));
 
+
+async function movieSearch(event) {
+  const OMDB_API = `http://www.omdbapi.com/?s=${event.srcElement.value}&apikey=8bf835a0`;
+
+
+    await fetch(OMDB_API)
+    .then((response) => response.json())
+    .then((data) => {
+        const movies = data.Search;
+        movies.forEach((movie) => 
+        document.getElementById("Search").innerHTML = movies.Poster);
+
+      //const moviesGrid = document.getElementById("grid");
+      //tableContainer.innerHTML = table.genTable();
+  
+} )
+}
